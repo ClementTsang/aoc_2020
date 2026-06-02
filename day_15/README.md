36c6949aa0dfd3ebaa51b2408e65ae701f3a365a
ClementTsang <dev@cjhtsang.ca>
2026-06-02 02:31:29 -0400

    day 15

--- day_15/README.md
+++ day_15/README.md
@@ -1,1 +1,3 @@
 # Day 15
+
+Pretty easy today, kinda just brute forced part 2.
--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,3 +1,61 @@
+use helpers::*;
+use std::{collections::HashMap, fs, path::Path};
+
+fn solve(input: &Path, end: usize) -> usize {
+    let s = fs::read_to_string(input).unwrap();
+    let numbers = s
+        .trim()
+        .split(",")
+        .map(|v| v.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut prev = 0;
+    let mut seen = HashMap::new();
+
+    for i in 1..=end {
+        match numbers.get(i - 1) {
+            Some(num) => {
+                seen.insert(prev, i - 1);
+                prev = *num;
+            }
+            None => match seen.get(&prev) {
+                Some(&last_seen) => {
+                    seen.insert(prev, i - 1);
+                    prev = i - 1 - last_seen;
+                }
+                None => {
+                    seen.insert(prev, i - 1);
+                    prev = 0;
+                }
+            },
+        }
+    }
+
+    prev
+}
+
+fn part_1(input: &Path) -> usize {
+    solve(input, 2020)
+}
+
+fn part_2(input: &Path) -> usize {
+    solve(input, 30000000)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_input_file();
+
+    println!("Part one: {}", part_1(&input));
+    println!("Part two: {}", part_2(&input));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example() {
+        assert_eq!(part_1(Path::new("example.txt")), 436);
+        assert_eq!(part_2(Path::new("example.txt")), 175594);
+    }
 }
