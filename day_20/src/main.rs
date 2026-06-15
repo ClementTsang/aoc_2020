f9f0756e179f9f0993e6f6ffdd50fd33fdc29cfe
ClementTsang <dev@cjhtsang.ca>
2026-06-14 21:39:30 -0400

    initialize other days

--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -1,3 +1,28 @@
+use helpers::*;
+use std::path::Path;
+
+fn part_1(input: &Path) -> u64 {
+    0
+}
+
+fn part_2(input: &Path) -> u64 {
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_input_file();
+
+    println!("Part one: {}", part_1(&input));
+    println!("Part two: {}", part_2(&input));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example() {
+        assert_eq!(part_1(Path::new("example.txt")), 0);
+        assert_eq!(part_2(Path::new("example.txt")), 0);
+    }
 }
--- day_25/src/main.rs
+++ day_25/src/main.rs
@@ -1,3 +1,22 @@
+use helpers::*;
+use std::path::Path;
+
+fn part_1(input: &Path) -> u64 {
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_input_file();
+
+    println!("Part one: {}", part_1(&input));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example() {
+        assert_eq!(part_1(Path::new("example.txt")), 0);
+    }
 }
